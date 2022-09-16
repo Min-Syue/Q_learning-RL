f60cbc8d9c8508b48818cccb39abe2a8c3477023
Min-Syue <minsyue0504@gmail.com>
2022-09-16 15:40:35 +0800

    Add Video GIF

--- model/funs.py
+++ model/funs.py
@@ -5,39 +5,31 @@ import numpy as np
 def agent_walk_path_vedio(env, q_table, times=1):
     
     # 宣告參數
-    output_epoch = []
-    total_reward = []
+    done = False
+    epochs = 0
+    reward_t = 0
 
-    for i in range(times):
+    env.reset()
 
-        done = False
-        epochs = 0
-        reward_t = 0
+    while not done:
 
-        env.reset()
-
-        while not done:
-
-            # 現在環境的代號
-            state = env.s
+        # 現在環境的代號
+        state = env.s
         
-            # 現在最大q值的action
-            action = np.argmax(q_table[state])
+        # 現在最大q值的action
+        action = np.argmax(q_table[state])
 
-            state, reward, done, _, _ = env.step(action)
+        state, reward, done, _, _ = env.step(action)
 
-            epochs += 1
-            reward_t += reward
+        epochs += 1
+        reward_t += reward
         
-        output_epoch.append(epochs)
-        total_reward.append(reward_t)
+    save_video(
+            env.render(),
+            "videos",
+            fps=env.metadata["render_fps"],
+            step_starting_index=0,
+            episode_index=0
+        )
 
-        save_video(
-                env.render(),
-                "videos",
-                fps=env.metadata["render_fps"],
-                step_starting_index=0,
-                episode_index=i
-            )
-
-    return output_epoch, total_reward
+    return epochs, reward_t
